c22df05cb3cf3f88bf60d772aba0f73cf5829658
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-01 22:08:26 +0000

    Add input event listener on textarea to countdown characters and change styling when get char limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  
+  $("textarea").on('input', function() {
+
+    // get access to counter class using dom traversal
+    const counter = $(this).parent().children(".form-footer").children(".counter");
+    
+    let currentLen = $(this).val().length;
+    let maxLen = 140;
+
+    // change class styling depending on input len
+    if (currentLen > 140) {
+      counter.addClass('warning').html(maxLen - currentLen);
+      
+    } else {
+      counter.removeClass('warning').html(maxLen - currentLen);
+    }
+  })
+});
