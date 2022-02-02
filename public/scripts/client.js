29ab28e2b43abc07298a454211a76cb0180eec28
Nastasi Davydov <nastasidavydov@gmail.com>
2022-02-02 19:20:55 +0000

    Create a function that takes in a tweet object and returns layout of tweet with data from that obj

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,67 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const tweetsData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1643583465541
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1643669865541
+  }
+];
+
+
+
+const createTweetElement = tweet => {
+  
+  const layout = ` 
+  <article class="tweet">
+  <header>
+    <div class="user-data">
+      <img src="${tweet.user.avatars}"/>
+      <span>${tweet.user.name}</span>
+    </div>
+    <div class="username">${tweet.user.handle}</div>
+  </header>
+
+  <div class="tweet-content">
+    <p>${tweet.content.text}</p>
+  </div>
+
+  <footer>
+    <div class="date">
+      <span>${tweet.created_at}</span>
+    </div>
+    <div class="icons">
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article>
+  `
+  return layout;
+};
+
+const $tweet = createTweetElement(tweetsData[0]);
+console.log($tweet);
+
+$(document).ready(function(){
+  $('#tweet-container').append($tweet)
+})
